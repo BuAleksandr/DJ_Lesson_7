09b151449a84c5639b2cd0da583249fc6ea86fad
BuAleksandr <87057144+BuAleksandr@users.noreply.github.com>
2022-01-31 00:05:45 +1100

    Fix filters, serializers, views,settings, add permissioins

---
+++ api_with_restrictions/setting_local.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'orm',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'USER': 'user_orm',
+        'PASSWORD': 'gfhjkm'
+    }
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'z5^+-9&b+ok(d%hl!%lbft$$bd(yz$3g5cq_ch6pw5l#8+ap+-'
